181eb34fb1607fcc842b91ed33c863c2c7cabdcc
Jayson Byrd <jayson_byrd_95@outlook.com>
2021-11-01 14:36:54 -0400

    Spinner Completed

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 /*
 SPINNER Instructions
 
@@ -40,20 +42,19 @@ STEP 4:
 import React from 'react'; /* STEP 0 */
 
 export default function Spinner() {
-/* STEP 1 */
-
+const [spinnerOn, setSpinnerOn] = useState(true);
   const toggleSpinner = () => {
-  /* STEP 4 */
+  setSpinnerOn(!spinnerOn);
   };
 
   return (
     <div className='widget-spinner container'>
       <h2>Spinner</h2>
       {
-        true && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
+        spinnerOn && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
       }
       <button id='toggleSpinner' onClick={toggleSpinner}>
-        Hide Spinner {/* STEP 3 */}
+         {spinnerOn ? 'hide spinner' : 'show spinner'}
       </button>
     </div>
   );
